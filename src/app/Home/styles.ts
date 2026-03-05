0b502202ee0cbc2d886395989a1039d3f4c29f69
Anderson Paiva <anderson.adolfo1998@gmail.com>
2026-03-05 13:46:23 -0300

    feat: criando mais componentes

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -6,10 +6,41 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: "#d0d2d8",
     paddingTop: 62,
-    padding: 24
   },
   logo: {
     height: 34,
     width: 134
+  },
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42
+  },
+  content: {
+    flex:1,
+    width: "100%",
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    paddingTop: 32,
+    padding: 24,
+    marginTop: 24
+  },
+  contentHeader: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E4E6EC",
+    paddingBottom: 12
+  },
+  clearButton: {
+    marginLeft: "auto"
+  },
+  clearText: {
+    fontSize: 12,
+    color: "#828282",
+    fontWeight: 600
   }
 });
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,21 @@
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { styles } from "./styles";
 
-export function Button() {
+type ButtonProps = TouchableOpacityProps & {
+    title: string;
+};
+
+export function Button({
+    title,
+    ...rest
+}: ButtonProps) {
     return (
         <TouchableOpacity 
             style={styles.container}
             activeOpacity={0.8}
+            {...rest}
         >
-            <Text style={styles.title}>Adicionar</Text>
+            <Text style={styles.title}>{title}</Text>
         </TouchableOpacity>
     )
 }
